faaed449b9b902d42f71aee6225b6e24d105b94e
polat türk <150281763+polatturk@users.noreply.github.com>
2024-04-12 20:49:24 +0300

    Add files via upload

---
+++ KontrolOdev/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace KontrolOdev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string kontrol1 = "polatturk.1";
+            string kontrol2 = "polat123";
+
+            Console.Write("Kullanıcı Adınız : ");
+            string kullaniciAdi = Console.ReadLine();
+
+            Console.Write("Şifreniz : ");
+            string sifre = Console.ReadLine();
+            
+
+
+            if (kullaniciAdi == kontrol1 && sifre == kontrol2) 
+            {
+                Console.WriteLine("Hoşgeldiniz...");
+                Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine("Hatalı kullanıcı adı veya şifre girdiniz Lütfen tekrar deneyiniz!");
+                Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
